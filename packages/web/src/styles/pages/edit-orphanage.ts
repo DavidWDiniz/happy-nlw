7c24b70ce3cc3c4fb03c31538218e3490a8f81f1
david <davidwdiniz@gmail.com>
2020-11-09 09:37:16 -0300

    Production server

--- packages/web/src/styles/pages/edit-orphanage.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const PageEditOrphanage = styled.div`
-    display: flex;
-    
-    @media(max-width: 720px) {
-        flex-direction: column;
-    }
-`;
--- packages/web/src/styles/pages/edit-pending-orphanage.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const PageCreateOrphanage = styled.div`
-    display: flex;
-    
-    @media(max-width: 720px) {
-        flex-direction: column;
-    }
-`;
